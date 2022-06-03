d10fc5951986b2a9855f64f6ff195ee7a64bfe91
Jerson Reyes <jerson.reyes.a@bulsu.edu.ph>
2022-06-04 02:04:29 +0800

    A

--- app/src/main/java/com/example/activity/MainActivity.java
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -5,8 +5,11 @@ import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -16,12 +19,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.VideoView;
+
 import java.util.ArrayList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
     EditText username, password;
     String username_string, password_string;
     boolean found = false;
@@ -32,45 +39,57 @@ public class MainActivity extends AppCompatActivity {
     //CREATE AN ARRAYLIST THAT WILL BE POPULATED BY DATA FROM THE ARRAYLIST FROM DBHANDLER SINGLETON
     ArrayList<String> login_cred = new ArrayList<String>();
 
-    //NEW CODE NEW CODE
-    /*
-        NEW CODE
-        NEW CODENEW CODENEW CODENEW CODE
-        NEW CODE
-        NEW CODE
-        NEW CODE
-        NEW CODE
-
-        NEW CODE
-         NEW CODE
-        NEW CODENEW CODENEW CODENEW CODE
-        NEW CODE
-        NEW CODE
-        NEW CODE
-    */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
 
         //RUNTIME SINGLETON IMPLEMENTATION TO MAKE DB ARRAYLIST ACCESSIBLE BY THE ENTIRE APP
         //GET INSTANCE - POPULATE LOGIN_CRED ARRAYLIST FROM THE DATA THAT IS STORED TO THE SINGLE ARRAYLIST FROM DBHANDLER SINGLETON
         login_cred = dbHandler.get().getAccounts();
 
-        AlertDialog.Builder alert = new AlertDialog.Builder(this)
-                .setTitle("BAGONG LAMAN NG ARRAYLIST")
-                .setMessage(login_cred.toString())
-                .setPositiveButton("Okay", null);
-        alert.show();
-
         //SET STATUSBAR TO TRANSAPRENT
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         setContentView(R.layout.activity_main);
 
+        VideoView videoview = (VideoView) findViewById(R.id.introbg);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.intro);
+        videoview.setDrawingCacheEnabled(true);
+        videoview.setVideoURI(uri);
+        videoview.start();
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                int videoWidth = mp.getVideoWidth();
+                int videoHeight = mp.getVideoHeight();
+
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                int screenWidth = displayMetrics.widthPixels;
+                int screenHeight = displayMetrics.heightPixels;
+
+                float scaleY = 1.0f;
+                float scaleX = (videoWidth * screenHeight / videoHeight) / screenWidth;
+
+                int pivotPointX = (int) (screenWidth / 2);
+                int pivotPointY = (int) (screenHeight / 2);
+
+                videoview.setScaleX(scaleX);
+                videoview.setScaleY(scaleY);
+                videoview.setPivotX(pivotPointX);
+                videoview.setPivotY(pivotPointY);
+
+                mp.setLooping(true);
+                mp.start();
+            }
+        });
+
         //ANIMATE
-        LinearLayout background, background_accent, login_ctr;
-        background_accent = findViewById(R.id.background_accent);
-        background = findViewById(R.id.background);
+        LinearLayout background, login_ctr;
+        TextView background_accent;
+        background_accent = findViewById(R.id.welcome2);
+        //background = findViewById(R.id.background);
         login_ctr = findViewById(R.id.login_ctr);
         TextView welcome = findViewById(R.id.welcome);
         ImageView logo = findViewById(R.id.logo);
@@ -86,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
         welcome.setAnimation(fade);
         logo.setAnimation(slide_bottom);
         background_accent.setAnimation(slide_left);
-        background.setAnimation(random);
+        //background.setAnimation(random);
         login_ctr.setAnimation(fade);
 
         username = findViewById(R.id.username);
